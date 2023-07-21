b38e08288e2b6b140c6ce80cc3c43c3313a9b3ac
casalecchi <felipevilela191@gmail.com>
2023-07-21 12:03:19 -0300

    chapter 1 finished

--- chapter1.swift
+++ chapter1.swift
@@ -1,26 +1,33 @@
-// Binary Search
-func binary_search(sorted_array: Array<Int>, item: Int) -> Int? {
-    var low = 0
-    var high = sorted_array.count - 1
+// Binary Search - O(log n)
+func binary_search(sorted_array: Array<Int>, item: Int) -> (Int?, Int) {
+    var low: Int = 0
+    var high: Int = sorted_array.count - 1
 
     var median: Int = 0
     var guess: Int = 0
+    var steps: Int = 0
 
     while low <= high {
         median = (high + low) / 2
         guess = sorted_array[median]
+        steps += 1
 
         if guess < item {
             low = median + 1
         } else if guess > item {
             high = median - 1
         } else {
-            return median
+            return (median, steps)
         }
     }
 
-    return nil
+    return (nil, 0)
 }
 
-let intArray: [Int] = Array(1...100)
-print(binary_search(sorted_array: intArray, item: 60) ?? 0)
+let intArray: [Int] = Array(1...256)
+let position: (Int?, Int) = binary_search(sorted_array: intArray, item: 61)
+if let index: Int = position.0 {
+    print("The item was found on index \(index). It took \(position.1) steps to find.")
+} else {
+    print("Item not found on Array.")
+}
